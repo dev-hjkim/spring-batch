cd204fd1c61a220c0937420aaea4055a03a2920a
dev-hjkim <dev--test@naver.com>
2022-11-22 23:33:25 +0900

    JobParameters의 유효성 검증 수행

--- src/main/java/com/example/batch/BatchApplication.java
+++ src/main/java/com/example/batch/BatchApplication.java
@@ -1,11 +1,14 @@
 package com.example.batch;
 
+import com.example.batch.validator.ParameterValidator;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.CompositeJobParametersValidator;
+import org.springframework.batch.core.job.DefaultJobParametersValidator;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +16,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @EnableBatchProcessing
 @SpringBootApplication
@@ -25,9 +30,29 @@ public class BatchApplication {
     private StepBuilderFactory stepBuilderFactory;
 
     @Bean
+    public CompositeJobParametersValidator validator() {
+        CompositeJobParametersValidator validator = new CompositeJobParametersValidator();
+
+        DefaultJobParametersValidator defaultJobParametersValidator = new DefaultJobParametersValidator(
+                new String[] {"fileName"},
+                new String[] {"name"}
+        );
+
+        defaultJobParametersValidator.afterPropertiesSet();
+
+        validator.setValidators(
+                Arrays.asList(new ParameterValidator(),
+                        defaultJobParametersValidator)
+        );
+
+        return validator;
+    }
+
+    @Bean
     public Job job() {
         return this.jobBuilderFactory.get("basicJob")
                 .start(step1())
+                .validator(validator())
                 .build();
     }
 
