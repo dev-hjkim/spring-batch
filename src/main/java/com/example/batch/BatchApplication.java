cb4ff455d8487ed5eb37b1a4782885e006fdec0b
dev-hjkim <dev--test@naver.com>
2022-11-26 23:29:51 +0900

    CallableTaskletAdapter

--- src/main/java/com/example/batch/BatchApplication.java
+++ src/main/java/com/example/batch/BatchApplication.java
@@ -14,12 +14,15 @@ import org.springframework.batch.core.job.CompositeJobParametersValidator;
 import org.springframework.batch.core.job.DefaultJobParametersValidator;
 import org.springframework.batch.core.listener.ExecutionContextPromotionListener;
 import org.springframework.batch.core.listener.JobListenerFactoryBean;
+import org.springframework.batch.core.step.tasklet.CallableTaskletAdapter;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import java.util.Arrays;
+import java.util.concurrent.Callable;
 
 @EnableBatchProcessing
 @SpringBootApplication
@@ -53,7 +56,7 @@ public class BatchApplication {
     @Bean
     public Job job() {
         return this.jobBuilderFactory.get("basicJob")
-                .start(step1())
+                .start(callableStep())
                 .validator(validator())
                 .incrementer(new DailyJobTimestamper())
                 .listener(JobListenerFactoryBean.getListener(
@@ -77,6 +80,31 @@ public class BatchApplication {
         listener.setKeys(new String[] {"name"});
 
         return listener;
+    }
+
+    @Bean
+    public Step callableStep() {
+        return this.stepBuilderFactory.get("callableStep")
+                .tasklet(tasklet())
+                .build();
+    }
+
+    @Bean
+    public Callable<RepeatStatus> callableObject() {
+        return () -> {
+            System.out.println("This was executed in another thread");
+            return RepeatStatus.FINISHED;
+        };
+    }
+
+    @Bean
+    public CallableTaskletAdapter tasklet() {
+        CallableTaskletAdapter callableTaskletAdapter =
+                new CallableTaskletAdapter();
+
+        callableTaskletAdapter.setCallable(callableObject());
+
+        return callableTaskletAdapter;
     }
 
     public static void main(String[] args) {
