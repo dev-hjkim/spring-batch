2e64c8dadcbe448d1a97aed1db6c9bf677491303
dev-hjkim <dev--test@naver.com>
2022-11-27 18:15:29 +0900

    decider 적용을 통한 다음 스텝 진행

--- src/main/java/com/example/batch/BatchApplication.java
+++ src/main/java/com/example/batch/BatchApplication.java
@@ -1,44 +1,18 @@
 package com.example.batch;
 
-import com.example.batch.incrementer.DailyJobTimestamper;
-import com.example.batch.listener.JobLoggerListener;
-import com.example.batch.listener.LoggingStepStartStopListener;
-import com.example.batch.service.CustomService;
-import com.example.batch.step.policy.RandomChunkSizePolicy;
-import com.example.batch.step.tasklet.HelloWorld;
-import com.example.batch.validator.ParameterValidator;
+import com.example.batch.step.decider.RandomDecider;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepExecutionListener;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepScope;
-import org.springframework.batch.core.job.CompositeJobParametersValidator;
-import org.springframework.batch.core.job.DefaultJobParametersValidator;
-import org.springframework.batch.core.listener.ExecutionContextPromotionListener;
-import org.springframework.batch.core.listener.JobListenerFactoryBean;
-import org.springframework.batch.core.step.tasklet.CallableTaskletAdapter;
-import org.springframework.batch.core.step.tasklet.MethodInvokingTaskletAdapter;
-import org.springframework.batch.core.step.tasklet.SystemCommandTasklet;
-import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.support.ListItemReader;
-import org.springframework.batch.repeat.CompletionPolicy;
+import org.springframework.batch.core.job.flow.JobExecutionDecider;
+import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.batch.repeat.policy.CompositeCompletionPolicy;
-import org.springframework.batch.repeat.policy.SimpleCompletionPolicy;
-import org.springframework.batch.repeat.policy.TimeoutTerminationPolicy;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.UUID;
-import java.util.concurrent.Callable;
 
 @EnableBatchProcessing
 @SpringBootApplication
@@ -51,175 +25,68 @@ public class BatchApplication {
     private StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public CompositeJobParametersValidator validator() {
-        CompositeJobParametersValidator validator = new CompositeJobParametersValidator();
+    public Tasklet passTasklet() {
+        return ((contribution, chunkContext) -> {
+            return RepeatStatus.FINISHED;
+        });
+    }
 
-        DefaultJobParametersValidator defaultJobParametersValidator = new DefaultJobParametersValidator(
-                new String[] {"fileName"},
-                new String[] {"name", "currentDate", "message"}
-        );
+    @Bean
+    public Tasklet successTasklet() {
+        return ((contribution, chunkContext) -> {
+            System.out.println("Success!");
+            return RepeatStatus.FINISHED;
+        });
+    }
 
-        defaultJobParametersValidator.afterPropertiesSet();
-
-        validator.setValidators(
-                Arrays.asList(new ParameterValidator(),
-                        defaultJobParametersValidator)
-        );
-
-        return validator;
+    @Bean
+    public Tasklet failTasklet() {
+        return ((contribution, chunkContext) -> {
+            System.out.println("Failure!");
+            return RepeatStatus.FINISHED;
+        });
     }
 
     @Bean
     public Job job() {
-        return this.jobBuilderFactory.get("basicJob")
-                .start(chunkStep())
-                .validator(validator())
-                .incrementer(new DailyJobTimestamper())
-                .listener(JobListenerFactoryBean.getListener(
-                        new JobLoggerListener()))
+        return this.jobBuilderFactory.get("conditionalJob")
+                .start(firstStep())
+                .next(decider())
+                .from(decider())
+                .on("FAILED").to(failureStep())
+                .from(decider())
+                .on("*").to(successStep())
+                .end()
                 .build();
     }
 
     @Bean
-    public Step step1() {
-        return this.stepBuilderFactory.get("step1")
-                .tasklet(new HelloWorld())
-                .listener(promotionListener())
+    public Step firstStep() {
+        return this.stepBuilderFactory.get("firstStep")
+                .tasklet(passTasklet())
                 .build();
     }
 
     @Bean
-    public Step callableStep() {
-        return this.stepBuilderFactory.get("callableStep")
-                .tasklet(tasklet())
-                .build();
-    }
-
-
-    @Bean
-    public Step methodInvokingStep() {
-        return this.stepBuilderFactory.get("methodInvokingStep")
-                .tasklet(methodInvokingTasklet(null))
+    public Step successStep() {
+        return this.stepBuilderFactory.get("successStep")
+                .tasklet(successTasklet())
                 .build();
     }
 
     @Bean
-    public Step systemCommandStep() {
-        return this.stepBuilderFactory.get("systemCommandStep")
-                .tasklet(systemCommandTasklet())
+    public Step failureStep() {
+        return this.stepBuilderFactory.get("failureStep")
+                .tasklet(failTasklet())
                 .build();
     }
 
     @Bean
-    public Step chunkStep() {
-        return this.stepBuilderFactory.get("chunkStep")
-                .<String, String>chunk(randomCompletionPolicy())
-                .reader(itemReader())
-                .writer(itemWriter())
-                .listener(new LoggingStepStartStopListener())
-                .build();
-    }
-
-    @Bean
-    public StepExecutionListener promotionListener() {
-        ExecutionContextPromotionListener listener
-                = new ExecutionContextPromotionListener();
-
-        listener.setKeys(new String[] {"name"});
-
-        return listener;
-    }
-
-    @Bean
-    public CallableTaskletAdapter tasklet() {
-        CallableTaskletAdapter callableTaskletAdapter =
-                new CallableTaskletAdapter();
-
-        callableTaskletAdapter.setCallable(callableObject());
-
-        return callableTaskletAdapter;
-    }
-
-    @Bean
-    public Callable<RepeatStatus> callableObject() {
-        return () -> {
-            System.out.println("This was executed in another thread");
-            return RepeatStatus.FINISHED;
-        };
-    }
-
-    @StepScope
-    @Bean
-    public MethodInvokingTaskletAdapter methodInvokingTasklet(
-            @Value("#{jobParameters['message']}") String message
-    ) {
-        MethodInvokingTaskletAdapter methodInvokingTaskletAdapter =
-                new MethodInvokingTaskletAdapter();
-
-        methodInvokingTaskletAdapter.setTargetObject(service());
-        methodInvokingTaskletAdapter.setTargetMethod("serviceMethod");
-        methodInvokingTaskletAdapter.setArguments(new String[] {message});
-
-        return methodInvokingTaskletAdapter;
-    }
-
-    @Bean
-    public SystemCommandTasklet systemCommandTasklet() {
-        SystemCommandTasklet systemCommandTasklet = new SystemCommandTasklet();
-
-        systemCommandTasklet.setCommand("rm -rf /tmp.txt");
-        systemCommandTasklet.setTimeout(5000);
-        systemCommandTasklet.setInterruptOnCancel(true);
-
-        return systemCommandTasklet;
-    }
-
-    @Bean
-    public ListItemReader<String> itemReader() {
-        List<String> items = new ArrayList<>(100000);
-
-        for (int i=0; i<100000; i++) {
-            items.add(UUID.randomUUID() + " ::: " + i + " 번째");
-        }
-        return new ListItemReader<>(items);
-    }
-
-    @Bean
-    public ItemWriter<String> itemWriter() {
-        return items -> {
-            for (String item : items) {
-                System.out.println(">> current item = " + item);
-            }
-            System.out.println("==========================");
-        };
-    }
-
-    @Bean
-    public CompletionPolicy completionPolicy() {
-        CompositeCompletionPolicy policy = new CompositeCompletionPolicy();
-
-        policy.setPolicies(
-                new CompletionPolicy[] {
-                        new TimeoutTerminationPolicy(3),
-                        new SimpleCompletionPolicy(1000)
-                }
-        );
-
-        return policy;
-    }
-
-    @Bean
-    public CustomService service() {
-        return new CustomService();
-    }
-
-    @Bean
-    public CompletionPolicy randomCompletionPolicy() {
-        return new RandomChunkSizePolicy();
+    public JobExecutionDecider decider() {
+        return new RandomDecider();
     }
 
     public static void main(String[] args) {
         SpringApplication.run(BatchApplication.class, args);
     }
-
 }
