ed02f3fea6824eb71a91f4db13b591d355e4205b
dev-hjkim <dev--test@naver.com>
2022-12-08 01:12:30 +0900

    JobParametersIncrementer 활성화

--- src/main/java/com/example/batch/controller/JobLaunchingController.java
+++ src/main/java/com/example/batch/controller/JobLaunchingController.java
@@ -3,6 +3,9 @@ package com.example.batch.controller;
 import com.example.batch.dto.JobLaunchRequest;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.explore.JobExplorer;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
@@ -18,11 +21,19 @@ public class JobLaunchingController {
     @Autowired
     private ApplicationContext context;
 
+    @Autowired
+    private JobExplorer jobExplorer;
+
     @PostMapping(path="/run")
     public ExitStatus runJob(@RequestBody JobLaunchRequest request) throws Exception {
         Job job = this.context.getBean(request.getName(), Job.class);
 
-        return this.jobLauncher.run(job, request.getJobParameters())
+        JobParameters jobParameters = new JobParametersBuilder(request.getJobParameters(),
+                this.jobExplorer)
+                .getNextJobParameters(job)
+                .toJobParameters();
+
+        return this.jobLauncher.run(job, jobParameters)
                 .getExitStatus();
     }
 
