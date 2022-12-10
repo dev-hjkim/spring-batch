6c065544bec90b11622293f7439f578d3b0727e4
dev-hjkim <dev--test@naver.com>
2022-12-10 23:29:20 +0900

    쿼츠 스케줄러로 잡 실행시키기

--- src/main/java/com/example/batch/BatchApplication.java
+++ src/main/java/com/example/batch/BatchApplication.java
@@ -1,55 +1,12 @@
 package com.example.batch;
 
-import com.example.batch.step.tasklet.ExploringTasklet;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.explore.JobExplorer;
-import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @EnableBatchProcessing
 @SpringBootApplication
 public class BatchApplication {
-
-    @Autowired
-    private JobBuilderFactory jobBuilderFactory;
-
-    @Autowired
-    private StepBuilderFactory stepBuilderFactory;
-
-    @Autowired
-    private JobExplorer jobExplorer;
-
-    @Bean
-    public Tasklet explorerTasklet() {
-        return new ExploringTasklet(this.jobExplorer);
-    }
-
-    @Bean
-    public Job job() {
-        return this.jobBuilderFactory.get("job")
-                .incrementer(new RunIdIncrementer())
-                .start(step1())
-                .build();
-    }
-
-    @Bean
-    public Step step1() {
-        return this.stepBuilderFactory.get("step1")
-                .tasklet((stepContribution, chunkContext) -> {
-                    System.out.println("step 1 ran today!");
-                    return RepeatStatus.FINISHED;
-                }).build();
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(BatchApplication.class, args);
     }
