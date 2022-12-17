da14d7348b399f625d06344ed6fb87032ffe060c
dev-hjkim <dev--test@naver.com>
2022-12-18 01:23:49 +0900

    고정 너비 파일을 위한 ItemReader

--- src/main/java/com/example/batch/BatchApplication.java
+++ src/main/java/com/example/batch/BatchApplication.java
@@ -1,31 +1,16 @@
 package com.example.batch;
 
-import com.example.batch.dao.TransactionDao;
-import com.example.batch.dao.TransactionDaoSupport;
-import com.example.batch.domain.AccountSummary;
-import com.example.batch.domain.Transaction;
-import com.example.batch.itemprocessor.TransactionApplierProcessor;
-import com.example.batch.itemreader.TransactionReader;
+import com.example.batch.domain.Customer;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
-import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
-import org.springframework.batch.item.database.JdbcBatchItemWriter;
-import org.springframework.batch.item.database.JdbcCursorItemReader;
-import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
-import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
+import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
-import org.springframework.batch.item.file.FlatFileItemWriter;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
-import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
-import org.springframework.batch.item.file.mapping.PassThroughFieldSetMapper;
-import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
-import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
-import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
-import org.springframework.batch.item.file.transform.FieldSet;
+import org.springframework.batch.item.file.transform.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -33,7 +18,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.Resource;
 
-import javax.sql.DataSource;
 
 @EnableBatchProcessing
 @SpringBootApplication
@@ -46,11 +30,18 @@ public class BatchApplication {
     private StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Job transactionJob() {
-        return this.jobBuilderFactory.get("transactionJob")
-                .start(importTransactionFileStep())
-                .next(applyTransactionStep())
-                .next(generateAccountSummaryStep())
+    public Job job() {
+        return this.jobBuilderFactory.get("job")
+                .start(copyFileStep())
+                .build();
+    }
+
+    @Bean
+    public Step copyFileStep() {
+        return this.stepBuilderFactory.get("copyFileStep")
+                .<Customer, Customer>chunk(10)
+                .reader(customerItemReader(null))
+                .writer(itemWriter())
                 .build();
     }
 
@@ -59,137 +50,24 @@ public class BatchApplication {
 
     @Bean
     @StepScope
-    public TransactionReader transactionReader() {
-        return new TransactionReader(fileItemReader(null));
-    }
-
-    @Bean
-    @StepScope
-    public FlatFileItemReader<FieldSet> fileItemReader(
-            @Value("#{jobParameters['transactionFile']") Resource inputFile
-    ) {
-        return new FlatFileItemReaderBuilder<FieldSet>()
-                .name("fileItemReader")
+    public FlatFileItemReader<Customer> customerItemReader(
+            @Value("#{jobParameters['customerFile']}") Resource inputFile) {
+        return new FlatFileItemReaderBuilder<Customer>()
+                .name("customerItemReader")
                 .resource(inputFile)
-                .lineTokenizer(new DelimitedLineTokenizer())
-                .fieldSetMapper(new PassThroughFieldSetMapper())
+                .fixedLength()
+                .columns(new Range[]{new Range(1,11), new Range(12,12), new Range(13,22),
+                new Range(23,26), new Range(27,46), new Range(47,62), new Range(63,64),
+                new Range(65,69)})
+                .names(new String[] {"firstName", "middleInitial", "lastName",
+                "addressNumber", "street", "city", "state", "zipCode"})
+                .targetType(Customer.class)
                 .build();
     }
 
     @Bean
-    public JdbcBatchItemWriter<Transaction> transactionWriter(DataSource dataSource) {
-        return new JdbcBatchItemWriterBuilder<Transaction>()
-                .itemSqlParameterSourceProvider(
-                        new BeanPropertyItemSqlParameterSourceProvider<>()
-                ).sql("INSERT INTO TRANSACTION " +
-                        "(ACCOUNT_SUMMARY_ID, TIMESTAMP, AMOUNT) " +
-                        "VALUES ((SELECT ID FROM ACCOUNT_SUMMARY " +
-                        "          WHERE ACCOUNT_NUMBER = :accountNumber), " +
-                        ":timestamp, :amount)")
-                .dataSource(dataSource)
-                .build();
-    }
-
-    @Bean
-    public Step importTransactionFileStep() {
-        return this.stepBuilderFactory.get("importTransactionFileStep")
-                .startLimit(2)
-                .<Transaction, Transaction> chunk(100)
-                .reader(transactionReader())
-                .writer(transactionWriter(null))
-                .allowStartIfComplete(true)
-                .listener(transactionReader())
-                .build();
-    }
-
-
-
-    //////////////////////////// STEP 2 ////////////////////////////
-
-    @Bean
-    @StepScope
-    public JdbcCursorItemReader<AccountSummary> accountSummaryReader(DataSource dataSource) {
-        return new JdbcCursorItemReaderBuilder<AccountSummary>()
-                .name("accountSummaryReader")
-                .dataSource(dataSource)
-                .sql("SELECT ACCOUNT_NUMBER, CURRENT_BALANCE " +
-                        "FROM ACCOUNT_SUMMARY A " +
-                        "WHERE A.ID IN (" +
-                        "      SELECT DISTINCT T.ACCOUNT_SUMMARY_ID " +
-                        "        FROM TRANSACTION T) " +
-                        "ORDER BY A.ACCOUNT_NUMBER")
-                .rowMapper((resultSet, rowNumber) -> {
-                    AccountSummary summary = new AccountSummary();
-
-                    summary.setAccountNumber(resultSet.getString("account_number"));
-                    summary.setCurrentBalance(resultSet.getDouble("current_balance"));
-
-                    return summary;
-                }).build();
-    }
-
-    @Bean
-    public TransactionDao transactionDao(DataSource dataSource) {
-        return new TransactionDaoSupport(dataSource);
-    }
-
-    @Bean
-    public TransactionApplierProcessor transactionApplierProcessor() {
-        return new TransactionApplierProcessor(transactionDao(null));
-    }
-
-    @Bean
-    public JdbcBatchItemWriter<AccountSummary> accountSummaryWriter(DataSource dataSource) {
-        return new JdbcBatchItemWriterBuilder<AccountSummary>()
-                .dataSource(dataSource)
-                .itemSqlParameterSourceProvider(
-                        new BeanPropertyItemSqlParameterSourceProvider<>())
-                .sql("UPDATE ACCOUNT_SUMMARY " +
-                        "SET CURRENT_BALANCE = :currentBalance " +
-                        "WHERE ACCOUNT_NUMBER = :accountNumber"
-                ).build();
-    }
-
-    @Bean
-    public Step applyTransactionStep() {
-        return this.stepBuilderFactory.get("applyTransactionStep")
-                .<AccountSummary, AccountSummary> chunk(100)
-                .reader(accountSummaryReader(null))
-                .processor(transactionApplierProcessor())
-                .writer(accountSummaryWriter(null))
-                .build();
-    }
-
-
-    //////////////////////////// STEP 3 ////////////////////////////
-
-    @Bean
-    @StepScope
-    public FlatFileItemWriter<AccountSummary> accountSummaryFileWriter(
-            @Value("#{jobParameters['summaryFile']}") Resource summaryFile) {
-
-        DelimitedLineAggregator<AccountSummary> lineAggregator =
-                new DelimitedLineAggregator<>();
-        BeanWrapperFieldExtractor<AccountSummary> fieldExtractor =
-                new BeanWrapperFieldExtractor<>();
-        fieldExtractor.setNames(new String[] {"accountNumber", "currentBalance"});
-        fieldExtractor.afterPropertiesSet();
-        lineAggregator.setFieldExtractor(fieldExtractor);
-
-        return new FlatFileItemWriterBuilder<AccountSummary>()
-                .name("accountSummaryFileWriter")
-                .resource(summaryFile)
-                .lineAggregator(lineAggregator)
-                .build();
-    }
-
-    @Bean
-    public Step generateAccountSummaryStep() {
-        return this.stepBuilderFactory.get("generateAccountSummaryStep")
-                .<AccountSummary, AccountSummary>chunk(100)
-                .reader(accountSummaryReader(null))
-                .writer(accountSummaryFileWriter(null))
-                .build();
+    public ItemWriter<Customer> itemWriter() {
+        return (items) -> items.forEach(System.out::println);
     }
 
     public static void main(String[] args) {
