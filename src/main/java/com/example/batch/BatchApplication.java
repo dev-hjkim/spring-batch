085203116ba66bfd8e9baef86e1d53f52227176b
dev-hjkim <dev--test@naver.com>
2023-01-04 23:00:00 +0900

    mongodb 사용하는 경우의 ItemReader

--- src/main/java/com/example/batch/BatchApplication.java
+++ src/main/java/com/example/batch/BatchApplication.java
@@ -1,8 +1,6 @@
 package com.example.batch;
 
 import com.example.batch.config.HibernateBatchConfigurer;
-import com.example.batch.domain.Customer;
-import com.example.batch.mapper.CustomerRowMapper;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
@@ -10,18 +8,18 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.database.StoredProcedureItemReader;
-import org.springframework.batch.item.database.builder.StoredProcedureItemReaderBuilder;
+import org.springframework.batch.item.data.MongoItemReader;
+import org.springframework.batch.item.data.builder.MongoItemReaderBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.core.ArgumentPreparedStatementSetter;
-import org.springframework.jdbc.core.SqlParameter;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoOperations;
 
-import javax.sql.DataSource;
-import java.sql.Types;
+import java.util.Collections;
+import java.util.Map;
 
 
 @EnableBatchProcessing
@@ -34,8 +32,6 @@ public class BatchApplication {
     @Autowired
     private StepBuilderFactory stepBuilderFactory;
 
-    @Autowired
-    private HibernateBatchConfigurer batchConfigurer;
 
     @Bean
     public Job job() {
@@ -47,8 +43,8 @@ public class BatchApplication {
     @Bean
     public Step copyFileStep() {
         return this.stepBuilderFactory.get("copyFileStep")
-                .<Customer, Customer>chunk(10)
-                .reader(customerItemReader(null, "Seoul"))
+                .<Map, Map>chunk(10)
+                .reader(tweetsItemReader(null, null))
                 .writer(itemWriter())
                 .build();
     }
@@ -58,25 +54,23 @@ public class BatchApplication {
 
     @Bean
     @StepScope
-    public StoredProcedureItemReader<Customer> customerItemReader(DataSource dataSource,
-                                                                  @Value("#{jobParameters['city']}") String city) {
+    public MongoItemReader<Map> tweetsItemReader(MongoOperations mongoTemplate,
+                                                 @Value("#{jobParameters['hashTag']}") String hashtag) {
 
-        return new StoredProcedureItemReaderBuilder<Customer>()
-                .name("customerItemReader")
-                .dataSource(dataSource)
-                .procedureName("customer_list")
-                .parameters(new SqlParameter[] {
-                        new SqlParameter("cityOption", Types.VARCHAR)
-                })
-                .preparedStatementSetter(
-                        new ArgumentPreparedStatementSetter(new Object[] {city})
-                )
-                .rowMapper(new CustomerRowMapper())
+        return new MongoItemReaderBuilder<Map>()
+                .name("tweetsItemReader")
+                .targetType(Map.class)
+                .jsonQuery("{ \"entities.hashtags.text\": { $eq: ?0}}")
+                .collection("tweets_collection")
+                .parameterValues(Collections.singletonList(hashtag))
+                .pageSize(10)
+                .sorts(Collections.singletonMap("created_at", Sort.Direction.ASC))
+                .template(mongoTemplate)
                 .build();
     }
 
     @Bean
-    public ItemWriter<Customer> itemWriter() {
+    public ItemWriter<Map> itemWriter() {
         return (items) -> items.forEach(System.out::println);
     }
 
