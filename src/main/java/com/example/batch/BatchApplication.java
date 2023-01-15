dedee8df82a9e8f6050eeb58cb96af72188a584f
dev-hjkim <dev--test@naver.com>
2023-01-15 16:44:17 +0900

    stepListener를 통한 에러 로깅

--- src/main/java/com/example/batch/BatchApplication.java
+++ src/main/java/com/example/batch/BatchApplication.java
@@ -2,7 +2,7 @@ package com.example.batch;
 
 import com.example.batch.domain.Customer;
 import com.example.batch.itemreader.CustomerItemReader;
-import com.example.batch.skipper.FileVerificationSkipper;
+import com.example.batch.listener.CustomerItemListener;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
@@ -10,7 +10,6 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -43,7 +42,9 @@ public class BatchApplication {
                 .reader(customerItemReader())
                 .writer(itemWriter())
                 .faultTolerant()
-                .skipPolicy(new FileVerificationSkipper())
+                .skipLimit(100)
+                .skip(Exception.class)
+                .listener(customerListener())
                 .build();
     }
 
@@ -62,6 +63,11 @@ public class BatchApplication {
     @Bean
     public ItemWriter<Customer> itemWriter() {
         return (items) -> items.forEach(System.out::println);
+    }
+
+    @Bean
+    public CustomerItemListener customerListener() {
+        return new CustomerItemListener();
     }
 
     public static void main(String[] args) {
