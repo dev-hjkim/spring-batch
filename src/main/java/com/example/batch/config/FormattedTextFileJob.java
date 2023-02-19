fec0b830ca99f4ef0b4c788f8faa5d6bf31e53bc
dev-hjkim <dev--test@naver.com>
2023-02-19 21:36:47 +0900

    JmsItemWriter

--- src/main/java/com/example/batch/config/FormattedTextFileJob.java
+++ src/main/java/com/example/batch/config/FormattedTextFileJob.java
@@ -1,27 +1,39 @@
 package com.example.batch.config;
 
-import com.example.batch.domain.JpaCustomer;
-import com.example.batch.repository.JpaCustomerRepository;
-import com.example.batch.service.LoggingService;
+import com.example.batch.domain.Customer2;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.item.adapter.PropertyExtractingDelegatingItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.jms.JmsItemReader;
+import org.springframework.batch.item.jms.JmsItemWriter;
+import org.springframework.batch.item.jms.builder.JmsItemReaderBuilder;
+import org.springframework.batch.item.jms.builder.JmsItemWriterBuilder;
+import org.springframework.batch.item.xml.StaxEventItemWriter;
+import org.springframework.batch.item.xml.builder.StaxEventItemWriterBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jms.connection.CachingConnectionFactory;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+import org.springframework.oxm.xstream.XStreamMarshaller;
+
+import javax.jms.ConnectionFactory;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @EnableBatchProcessing
 @Configuration
-@EnableJpaRepositories(basePackageClasses = JpaCustomerRepository.class)
 public class FormattedTextFileJob {
     private JobBuilderFactory jobBuilderFactory;
     private StepBuilderFactory stepBuilderFactory;
@@ -32,13 +44,34 @@ public class FormattedTextFileJob {
         this.stepBuilderFactory = stepBuilderFactory;
     }
 
+    @Bean
+    public MessageConverter jacksonJmsMessageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+
+    @Bean
+    public JmsTemplate jmsTemplate(ConnectionFactory connectionFactory) {
+        CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory(connectionFactory);
+        cachingConnectionFactory.afterPropertiesSet();
+
+        JmsTemplate jmsTemplate = new JmsTemplate(cachingConnectionFactory);
+        jmsTemplate.setDefaultDestinationName("customers");
+        jmsTemplate.setMessageConverter(jacksonJmsMessageConverter());
+        jmsTemplate.setReceiveTimeout(500L);
+
+        return jmsTemplate;
+    }
+
     //////////////////////////// STEP 1 ////////////////////////////
 
     @Bean
-    public FlatFileItemReader<JpaCustomer> customerFileReader() {
+    public FlatFileItemReader<Customer2> customerFileReader() {
         Resource inputFile = new ClassPathResource("input/customer.csv");
 
-        return new FlatFileItemReaderBuilder<JpaCustomer>()
+        return new FlatFileItemReaderBuilder<Customer2>()
                 .name("customerFileReader")
                 .delimited()
                 .names(new String[] {"firstName",
@@ -48,38 +81,67 @@ public class FormattedTextFileJob {
                         "city",
                         "state",
                         "zip"})
-                .targetType(JpaCustomer.class)
+                .targetType(Customer2.class)
                 .resource(inputFile)
                 .build();
     }
 
     @Bean
-    public PropertyExtractingDelegatingItemWriter<JpaCustomer> itemWriter(LoggingService loggingService) {
-        PropertyExtractingDelegatingItemWriter<JpaCustomer> itemWriter =
-                new PropertyExtractingDelegatingItemWriter<>();
+    public StaxEventItemWriter<Customer2> xmlOutputWriter() {
+        Resource outputFile = new FileSystemResource("output/jmsCustomer.xml");
 
-        itemWriter.setTargetObject(loggingService);
-        itemWriter.setTargetMethod("logCustomerAddress");
-        itemWriter.setFieldsUsedAsTargetMethodArguments(
-                new String[] {"address", "city", "state", "zip"}
-        );
+        Map<String, Class> aliases = new HashMap<>();
+        aliases.put("customer", Customer2.class);
 
-        return itemWriter;
-    }
+        XStreamMarshaller marshaller = new XStreamMarshaller();
+        marshaller.setAliases(aliases);
 
-    @Bean
-    public Step formatStep() throws Exception {
-        return this.stepBuilderFactory.get("formatStep")
-                .<JpaCustomer, JpaCustomer> chunk(10)
-                .reader(customerFileReader())
-                .writer(itemWriter(null))
+        return new StaxEventItemWriterBuilder<Customer2>()
+                .name("xmlOutputWriter")
+                .resource(outputFile)
+                .marshaller(marshaller)
+                .rootTagName("customers")
                 .build();
     }
 
     @Bean
-    public Job itemWriterAdapterFormatJob() throws Exception {
-        return this.jobBuilderFactory.get("itemWriterAdapterFormatJob")
-                .start(formatStep())
+    public JmsItemReader<Customer2> jmsItemReader(JmsTemplate jmsTemplate) {
+        return new JmsItemReaderBuilder<Customer2>()
+                .jmsTemplate(jmsTemplate)
+                .itemType(Customer2.class)
+                .build();
+    }
+
+    @Bean
+    public JmsItemWriter<Customer2> jmsItemWriter(JmsTemplate jmsTemplate) {
+        return new JmsItemWriterBuilder<Customer2>()
+                .jmsTemplate(jmsTemplate)
+                .build();
+    }
+
+    @Bean
+    public Step formatInputStep() throws Exception {
+        return this.stepBuilderFactory.get("formatInputStep")
+                .<Customer2, Customer2>chunk(10)
+                .reader(customerFileReader())
+                .writer(jmsItemWriter(null))
+                .build();
+    }
+
+    @Bean
+    public Step formatOutputStep() throws Exception {
+        return this.stepBuilderFactory.get("formatOutputStep")
+                .<Customer2, Customer2>chunk(10)
+                .reader(jmsItemReader(null))
+                .writer(xmlOutputWriter())
+                .build();
+    }
+
+    @Bean
+    public Job jmsFormatJob() throws Exception {
+        return this.jobBuilderFactory.get("jmsFormatJob")
+                .start(formatInputStep())
+                .next(formatOutputStep())
                 .incrementer(new RunIdIncrementer())
                 .build();
     }
