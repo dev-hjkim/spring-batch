1e9fdd05cf4d9173b838024a6923b33512d7590e
dev-hjkim <dev--test@naver.com>
2023-02-21 02:02:51 +0900

    MultiResourceItemWriter

--- src/main/java/com/example/batch/config/FormattedTextFileJob.java
+++ src/main/java/com/example/batch/config/FormattedTextFileJob.java
@@ -1,31 +1,35 @@
 package com.example.batch.config;
 
 import com.example.batch.domain.CustomerWithEmail;
+import com.example.batch.suffixCreator.CustomerOutputFileSuffixCreator;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.database.JdbcCursorItemReader;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.MultiResourceItemWriter;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
-import org.springframework.batch.item.mail.SimpleMailMessageItemWriter;
-import org.springframework.batch.item.mail.builder.SimpleMailMessageItemWriterBuilder;
+import org.springframework.batch.item.file.builder.MultiResourceItemWriterBuilder;
+import org.springframework.batch.item.xml.StaxEventItemWriter;
+import org.springframework.batch.item.xml.builder.StaxEventItemWriterBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.mail.MailSender;
-import org.springframework.mail.SimpleMailMessage;
+import org.springframework.oxm.xstream.XStreamMarshaller;
 
 import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @EnableBatchProcessing
@@ -33,11 +37,14 @@ import javax.sql.DataSource;
 public class FormattedTextFileJob {
     private JobBuilderFactory jobBuilderFactory;
     private StepBuilderFactory stepBuilderFactory;
+    private CustomerOutputFileSuffixCreator customerOutputFileSuffixCreator;
 
     public FormattedTextFileJob(JobBuilderFactory jobBuilderFactory,
-                                StepBuilderFactory stepBuilderFactory) {
+                                StepBuilderFactory stepBuilderFactory,
+                                CustomerOutputFileSuffixCreator customerOutputFileSuffixCreator) {
         this.jobBuilderFactory = jobBuilderFactory;
         this.stepBuilderFactory = stepBuilderFactory;
+        this.customerOutputFileSuffixCreator = customerOutputFileSuffixCreator;
     }
 
     //////////////////////////// STEP 1 ////////////////////////////
@@ -85,9 +92,31 @@ public class FormattedTextFileJob {
     }
 
     @Bean
-    public SimpleMailMessageItemWriter emailItemWriter(MailSender mailSender) {
-        return new SimpleMailMessageItemWriterBuilder()
-                .mailSender(mailSender)
+    public StaxEventItemWriter<CustomerWithEmail> delegateItemWriter() throws Exception {
+        Map<String, Class> aliases = new HashMap<>();
+        aliases.put("customer", CustomerWithEmail.class);
+
+        XStreamMarshaller marshaller = new XStreamMarshaller();
+        marshaller.setAliases(aliases);
+        marshaller.afterPropertiesSet();
+
+        return new StaxEventItemWriterBuilder<CustomerWithEmail>()
+                .name("customerItemWriter")
+                .marshaller(marshaller)
+                .rootTagName("customers")
+                .build();
+    }
+
+    @Bean
+    public MultiResourceItemWriter<CustomerWithEmail> multiCustomerFileWriter(
+            CustomerOutputFileSuffixCreator suffixCreator
+    ) throws Exception {
+        return new MultiResourceItemWriterBuilder<CustomerWithEmail>()
+                .name("multiCustomerFileWriter")
+                .delegate(delegateItemWriter())
+                .itemCountLimitPerResource(25)
+                .resource(new FileSystemResource("output/multiResourceCustomer"))
+                .resourceSuffixCreator(suffixCreator)
                 .build();
     }
 
@@ -101,29 +130,19 @@ public class FormattedTextFileJob {
     }
 
     @Bean
-    public Step emailStep() throws Exception {
-        return this.stepBuilderFactory.get("emailStep")
-                .<CustomerWithEmail, SimpleMailMessage>chunk(10)
+    public Step multiXmlGeneratorStep() throws Exception {
+        return this.stepBuilderFactory.get("multiXmlGeneratorStep")
+                .<CustomerWithEmail, CustomerWithEmail>chunk(10)
                 .reader(customerCursorItemReader(null))
-                .processor((ItemProcessor<CustomerWithEmail, SimpleMailMessage>) customer -> {
-                    SimpleMailMessage mail = new SimpleMailMessage();
-                    mail.setFrom("prospringbatch@gmail.com");
-                    mail.setTo(customer.getEmail());
-                    mail.setSubject("Welcome!");
-                    mail.setText(String.format("Welcome %s %s,\nYou were " +
-                            "imported into the system using Spring Batch!",
-                            customer.getFirstName(), customer.getLastName()));
-                    return mail;
-                } )
-                .writer(emailItemWriter(null))
+                .writer(multiCustomerFileWriter(customerOutputFileSuffixCreator))
                 .build();
     }
 
     @Bean
-    public Job emailJob() throws Exception {
-        return this.jobBuilderFactory.get("emailJob")
+    public Job xmlGeneratorJob() throws Exception {
+        return this.jobBuilderFactory.get("xmlGeneratorJob")
                 .start(importStep())
-                .next(emailStep())
+                .next(multiXmlGeneratorStep())
                 .incrementer(new RunIdIncrementer())
                 .build();
     }
---
+++ src/main/java/com/example/batch/suffixCreator/CustomerOutputFileSuffixCreator.java
@@ -0,0 +1,12 @@
+package com.example.batch.suffixCreator;
+
+import org.springframework.batch.item.file.ResourceSuffixCreator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomerOutputFileSuffixCreator implements ResourceSuffixCreator {
+    @Override
+    public String getSuffix(int arg0) {
+        return arg0 + ".xml";
+    }
+}
