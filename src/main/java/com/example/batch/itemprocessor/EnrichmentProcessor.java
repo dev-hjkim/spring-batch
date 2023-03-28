605da6ee011411b93f2ba2dc6b9bb9374a3e00c5
dev-hjkim <dev--test@naver.com>
2023-03-28 22:05:22 +0900

    circuitBreaker

--- src/main/java/com/example/batch/itemprocessor/EnrichmentProcessor.java
+++ src/main/java/com/example/batch/itemprocessor/EnrichmentProcessor.java
@@ -5,12 +5,21 @@ import org.springframework.batch.item.ItemProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.retry.annotation.CircuitBreaker;
+import org.springframework.retry.annotation.Recover;
 import org.springframework.web.client.RestTemplate;
 
 public class EnrichmentProcessor implements ItemProcessor<Foo, Foo> {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Recover
+    public Foo fallback(Foo foo) {
+        foo.setMessage("error");
+        return foo;
+    }
+
+    @CircuitBreaker(maxAttempts = 1)
     @Override
     public Foo process(Foo foo) throws Exception {
         ResponseEntity<String> responseEntity =
